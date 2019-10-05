19c5f44051e8a0ba84b32acc8c8fe830ee3c3fd7
keyber <keyvan.beroukhim@etu.upmc.fr>
2019-10-05 14:20:16 +0200

    log-scale-pca. todo train test

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pandas as pd
 
-def preprocessing(filename, threshold=0.4, nelement=None, verbose=False):
+def preprocessing(filename, threshold=0.4, nelement=None, remove_nones=True, return_x_y=True, allow_inexact=False, verbose=False):
     data = pd.read_csv(filename)
+    
+    if nelement and (allow_inexact or threshold>=1):
+        data = data[:nelement]
+    
     if verbose:
         print("Original data shape : {}".format(data.shape))
-
+    
     data['Label'] = np.where(data['Label']=='s', 1, 0)
     data = data.drop(columns=["KaggleSet","Weight","KaggleWeight", "EventId"])
-
+    
     columns_to_remove = []
     for column in data.columns:
         n_invalid = np.count_nonzero(data[column] == -999.0)
@@ -19,26 +23,31 @@ def preprocessing(filename, threshold=0.4, nelement=None, verbose=False):
             if verbose:
                 print("\tRemoving {}".format(column))
             columns_to_remove.append(column)
-
+    
     data = data.drop(columns=columns_to_remove)
-
-    seriesObj = data.apply(lambda x: -999.0 in list(x), axis=1)
+    
+    seriesObj = data.apply(lambda x_: -999.0 in list(x_), axis=1)
     numOfRows = len(seriesObj[seriesObj == False].index)
     if verbose:
         print("{} % of valid rows".format(numOfRows/data.shape[0]))
-
-    data = data[seriesObj == False]
+    
+    if remove_nones:
+        data = data[seriesObj == False]
+    else:
+        data = data.replace(-999, np.nan)
+    
     if nelement:
         data = data[:nelement]
-
-    y = np.array(data['Label'])
-    data = data.drop(columns=['Label'])
-    x = data.values
-
+    
     if verbose:
         print("Preprocessed data shape : {}".format(data.shape))
-
-    return x, y
+    
+    if not return_x_y:
+        return data
+    else:
+        y = data['Label']
+        x = data.drop(columns=['Label'])
+        return x, y
 
 def main():
     data = preprocessing('data.csv')
