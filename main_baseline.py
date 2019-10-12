77ee6b246170a6ec2fe06f89c75b268e34bcf092
laudao <nguyen.laura97@gmail.com>
2019-10-12 11:23:32 +0200

    - grid search avec l'AMS pondéré pour la baseline et les méthodes d'ensemble (n=1000) - test par cross-validation (n=1000) - écriture des résultats dans le rapport

--- main_baseline.py
+++ main_baseline.py
@@ -4,11 +4,9 @@ import pandas as pd
 from sklearn.experimental import enable_iterative_imputer  # noqa
 # noinspection PyUnresolvedReferences
 from sklearn.impute import SimpleImputer, IterativeImputer
-from sklearn.metrics import make_scorer
 from sklearn.utils import shuffle
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, AdaBoostClassifier
 from sklearn.decomposition import PCA
@@ -16,7 +14,6 @@ from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_sc
 from sklearn.linear_model import Perceptron  #, BayesianRidge
 from sklearn.base import BaseEstimator, TransformerMixin
 from time import time
-from AMS import AMS
 import matplotlib.pyplot as plt
 
 seed = 0
@@ -49,7 +46,8 @@ def compute_AMS(y_true, y_pred, weights):
 
 def grid_search(data, weights):
     X, y = data
-    C_values = np.logspace(-2, .5, num=5)
+    C_values = np.logspace(-2, 1, num=5)
+    results = []
 
     for C in C_values:
         for kernel in ['poly', 'rbf']:
@@ -66,27 +64,40 @@ def grid_search(data, weights):
                 score = compute_AMS(y_test, y_pred, weights[test_index])
                 scores_param.append(score)
 
-            print("C={}, kernel={}, mean AMS={}".format(C, kernel, np.mean(scores_param)))
+            results.append([C, kernel, np.mean(scores_param), np.std(scores_param)])
 
+    return pd.DataFrame.from_records(results, columns=['C', 'kernel', 'average', 'std'])
 
-def plot_score(scores, interval, scoring_function):
+def eval_best(data, weights, clf):
+    X, y = data
+
+    kf = KFold(n_splits=3)
+    scores_param = []
+
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        score = compute_AMS(y_test, y_pred, weights[test_index])
+        scores_param.append(score)
+
+    return np.mean(scores_param), np.std(scores_param)
+
+def plot_score(scores, interval):
     ax = plt.gca()
     ax.plot(interval, scores)
     ax.set_xscale('log')
     plt.xlabel('C')
-    if scoring_function == 'accuracy':
-        plt.ylabel('Accuracy')
-    elif scoring_function == 'f1':
-        plt.ylabel('Score F1')
-    else:
-        plt.ylabel('Score AMS')
+    plt.ylabel('AMS')
     plt.axis('tight')
     plt.show()
 
 def main():
-    n_train = 100
-    n_test = 100
-    RFnan = True
+    eval_mode = True
+    n_train = 1000
+    n_test = 1000
     t0 = time()
     data = shuffle(pd.read_csv('data.csv'), random_state=seed)[:n_train + n_test]
     y = data['Label']
@@ -100,18 +111,22 @@ def main():
     cols_log = ["DER_mass_MMC", "DER_mass_transverse_met_lep", "DER_mass_vis", "DER_pt_h", "DER_pt_ratio_lep_tau",
                 "DER_pt_tot", "DER_sum_pt", "PRI_jet_all_pt", "PRI_lep_pt", "PRI_met", "PRI_met_sumet", "PRI_tau_pt"]
     x = make_column_transformer((Shift_log(), cols_log), remainder="passthrough").fit_transform(x)
-    if RFnan:
-        x = StandardScaler().fit_transform(x)
-        x = SimpleImputer(missing_values=np.nan, fill_value=-999999.0).fit_transform(x)
-    else:
-        x = IterativeImputer(max_iter=int(1e2)).fit_transform(x)
-        x = StandardScaler().fit_transform(x)
-        # x = PCA(15).fit_transform(x)
+    x = IterativeImputer(max_iter=int(1e2)).fit_transform(x)
+    x = StandardScaler().fit_transform(x)
+    # x = PCA(15).fit_transform(x)
     X_train, X_test, y_train, y_test, weights_train, weights_test = train_test_split(x, y, weights, random_state=seed,
                                                                                      test_size=n_test)
 
-    # eval
-    grid_search((X_train, y_train), weights_train)
+    # train
+    start = time()
+    if not eval_mode:
+        results = grid_search((X_train, y_train), weights_train)
+        print(results)
+        print("\tBest results : \n\t{}".format(results.ix[results['average'].idxmax()]))
+    else:
+        clf = SVC(gamma="auto", max_iter=100000, C=1.78, kernel='rbf')
+        print(eval_best((X_test, y_test), weights_test, clf))
+    print("Total time : {}".format(time() - start))
 
 
 if __name__ == '__main__':
---
+++ main_ensemble.py
@@ -0,0 +1,194 @@
+import numpy as np
+import pandas as pd
+# noinspection PyUnresolvedReferences
+from sklearn.experimental import enable_iterative_imputer  # noqa
+# noinspection PyUnresolvedReferences
+from sklearn.impute import SimpleImputer, IterativeImputer
+from sklearn.utils import shuffle
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, AdaBoostClassifier
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold
+from sklearn.linear_model import Perceptron  #, BayesianRidge
+from sklearn.base import BaseEstimator, TransformerMixin
+from time import time
+import matplotlib.pyplot as plt
+
+seed = 0
+
+class Shift_log(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.shift = None
+
+    # noinspection PyUnusedLocal
+    def fit(self, x, y=None):
+        self.shift = 1 - np.nanmin(x, axis=0)
+
+        return self
+
+    def transform(self, x):
+        return np.log(x + self.shift)
+
+
+def compute_AMS(y_true, y_pred, weights):
+    # true positive
+    s = weights[np.where((y_pred == True) & (y_true == True))].sum()
+
+    # false positive
+    b = weights[np.where((y_pred == True) & (y_true == False))].sum()
+
+    br = 10
+
+    radicand = 2 * ((s + b + br) * np.log(1.0 + s / (b + br)) - s)
+    return np.sqrt(radicand)
+
+def grid_search(data, weights):
+    n_estimators_values = [None, 500, 1000, 2000]
+    X, y = data
+    results_bagging = []
+    results_boosting = []
+
+    for n_estimators in n_estimators_values:
+        if n_estimators:
+            bagging = BaggingClassifier(Perceptron(max_iter=1000), max_samples=0.5, max_features=0.5,
+                                        n_estimators=n_estimators)
+            boosting = AdaBoostClassifier(n_estimators=n_estimators)
+        else:
+            bagging = BaggingClassifier(Perceptron(max_iter=1000), max_samples=0.5, max_features=0.5)
+            boosting = AdaBoostClassifier()
+
+        kf = KFold(n_splits=3)
+        scores_bagging = []
+        scores_boosting = []
+
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+
+            bagging.fit(X_train, y_train)
+            y_pred = bagging.predict(X_test)
+            score = compute_AMS(y_test, y_pred, weights[test_index])
+            scores_bagging.append(score)
+
+
+            boosting.fit(X_train, y_train)
+            y_pred = boosting.predict(X_test)
+            score = compute_AMS(y_test, y_pred, weights[test_index])
+            scores_boosting.append(score)
+
+        results_bagging.append([n_estimators, np.mean(scores_bagging), np.std(scores_bagging)])
+        results_boosting.append([n_estimators, np.mean(scores_boosting), np.std(scores_boosting)])
+
+    return pd.DataFrame.from_records(results_bagging, columns=['n_estimators', 'average', 'std']),\
+           pd.DataFrame.from_records(results_boosting, columns=['n_estimators', 'average', 'std'])
+
+
+def grid_search_rf(data, weights):
+    n_estimators_values = [None, 500, 1000, 2000]
+    max_depth_values = [20, 50, None]
+    X, y = data
+    results = []
+
+    for n_estimators in n_estimators_values:
+        for max_depth in max_depth_values:
+            if n_estimators:
+                rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+            else:
+                rf = RandomForestClassifier(max_depth=max_depth)
+
+            kf = KFold(n_splits=3)
+            scores_param = []
+
+            for train_index, test_index in kf.split(X):
+                X_train, X_test = X[train_index], X[test_index]
+                y_train, y_test = y[train_index], y[test_index]
+
+                rf.fit(X_train, y_train)
+                y_pred = rf.predict(X_test)
+                score = compute_AMS(y_test, y_pred, weights[test_index])
+                scores_param.append(score)
+
+            results.append([n_estimators, max_depth, np.mean(scores_param), np.std(scores_param)])
+
+    return pd.DataFrame.from_records(results, columns=['n_estimators', 'max_depth', 'average', 'std'])
+
+def eval_best(data, weights, clf):
+    X, y = data
+
+    kf = KFold(n_splits=3)
+    scores_param = []
+
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        score = compute_AMS(y_test, y_pred, weights[test_index])
+        scores_param.append(score)
+
+    return np.mean(scores_param), np.std(scores_param)
+
+def main():
+    eval_mode = True
+    n_train = 1000
+    n_test = 1000
+    RFnan = False
+    t0 = time()
+    data = shuffle(pd.read_csv('data.csv'), random_state=seed)[:n_train + n_test]
+    y = data['Label']
+    y = np.where(y == 's', 1, 0)
+    x = data.drop(columns=['Label', "KaggleSet", "KaggleWeight", "EventId"])
+    weights = data['Weight'].values
+    x = x.drop(columns=['Weight'])
+    x = x.replace(-999, np.nan)
+
+    # preprocess
+    cols_log = ["DER_mass_MMC", "DER_mass_transverse_met_lep", "DER_mass_vis", "DER_pt_h", "DER_pt_ratio_lep_tau",
+                "DER_pt_tot", "DER_sum_pt", "PRI_jet_all_pt", "PRI_lep_pt", "PRI_met", "PRI_met_sumet", "PRI_tau_pt"]
+    x = make_column_transformer((Shift_log(), cols_log), remainder="passthrough").fit_transform(x)
+    if RFnan:
+        x = StandardScaler().fit_transform(x)
+        x = SimpleImputer(missing_values=np.nan, fill_value=-999999.0).fit_transform(x)
+    else:
+        x = IterativeImputer(max_iter=int(1e2)).fit_transform(x)
+        x = StandardScaler().fit_transform(x)
+        # x = PCA(15).fit_transform(x)
+    X_train, X_test, y_train, y_test, weights_train, weights_test = train_test_split(x, y, weights, random_state=seed,
+                                                                                     test_size=n_test)
+
+    # eval
+    start = time()
+    if not eval_mode:
+        if RFnan:
+            results = grid_search_rf((X_train, y_train), weights_train)
+            print("RF nan : \n\t{}".format(results))
+            print("\tBest results : \n\t{}".format(results.ix[results['average'].idxmax()]))
+        else:
+            results_bagging, results_boosting = grid_search((X_train, y_train), weights_train)
+            results_rf = grid_search_rf((X_train, y_train), weights_train)
+            print("Bagging : \n\t{}".format(results_bagging))
+            print("Boosting : \n\t{}".format(results_boosting))
+            print("RF : \n\t{}".format(results_rf))
+    else:
+        if RFnan:
+            rf_nan = RandomForestClassifier(n_estimators=500, max_depth=None)
+            average, std = eval_best((X_test, y_test), weights_test, rf_nan)
+            print("RFNan : %.4f +/- %.4f" % (average, std))
+        else:
+            rf = RandomForestClassifier(n_estimators=1000, max_depth=50)
+            bagging = BaggingClassifier(Perceptron(max_iter=1000), max_samples=0.5, max_features=0.5, n_estimators=2000)
+            boosting = AdaBoostClassifier(n_estimators=50)
+
+            for clf in [rf, bagging, boosting]:
+                average, std = eval_best((X_test, y_test), weights_test, clf)
+                print(clf)
+                print("%.4f +/- %.4f" % (average, std))
+    print("Total time : {}".format(time() - start))
+
+
+if __name__ == '__main__':
+    main()
+
